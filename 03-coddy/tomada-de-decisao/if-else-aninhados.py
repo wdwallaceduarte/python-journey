28d2f708ea4b8785f8b06bbd3e7986ae0ac2c48d
Wallace Duarte <wd.wallaceduarte@gmail.com>
2026-09-23 20:36:20 -0300

    fix(coddy): finalização do desafio if-else aninhados

--- 03-coddy/tomada-de-decisao/if-else-aninhados.py
+++ 03-coddy/tomada-de-decisao/if-else-aninhados.py
@@ -1,10 +1,40 @@
 age = int(input("Digite sua idade: "))
 
-with_parent = input() == 'ture'
+with_parent = input() == 'true'
 
 message = 'None'
 
-if age > 17:
-    message = 'You con watch any movie'
+if age >= 18:
+    message = "You can watch any movie"
 else:
-    if age < 18:
+    if with_parent:  
+        message = "You can watch PG-13 movies"
+    else:
+        message = "You can only watch G-rated movies"
+
+print(message)
+
+b1 = 5
+b2 = 2
+b3 = not((b1 + b2) > (b1 * b2))
+
+#====================================================
+
+# Note que x e y devem conter números inteiros positivos ou negativos.
+
+print(f'b3 = {b3}')
+print('='*45)
+print(""" 
+    ENQUANTO (z) NÃO FOR TRUE REPETE 
+z = ((x * y) < (x + y) && (x + y) < (x - y))
+""")
+print('='*45)
+z = ''
+
+while z != True:
+    x = int(input('x: '))
+    y = int(input('y: '))
+
+    z = ((x * y) < (x + y) and (x + y) < (x - y))
+
+    print(f'\n> z: {z}')
